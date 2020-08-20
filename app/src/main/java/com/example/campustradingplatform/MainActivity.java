49beb78d6e0a698e434ee155c92e8ef714be379b
kongrui0414 <2361640318@qq.com>
2020-08-20 11:07:13 +0800

    test:消息按钮点击事件

--- app/src/main/java/com/example/campustradingplatform/MainActivity.java
+++ app/src/main/java/com/example/campustradingplatform/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.campustradingplatform;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTabHost;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button messagebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        messagebtn=(Button)findViewById(R.id.btn1);
+        messagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MsgListActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
