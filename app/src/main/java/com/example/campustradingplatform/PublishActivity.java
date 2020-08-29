ce44a066c05f6b441c85d0f6831604fc37fe8735
kongrui0414 <2361640318@qq.com>
2020-08-29 20:37:39 +0800

    init：发布界面以及icon

---
+++ app/src/main/java/com/example/campustradingplatform/PublishActivity.java
@@ -0,0 +1,13 @@
+package com.example.campustradingplatform;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PublishActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.public_fragment);
+    }
+}
